90991d7c92942c983e4ceb0fd0ebff5f78178bd8
Ejdeguzman72 <ejdeguzman72@gmail.com>
2020-06-22 00:39:58 -0500

    Controllers

---
+++ src/main/java/com/DeGuzmanFamilyAPI/DeGuzmanFamilyAPIBackend/controller/AutoTransactionController.java
@@ -0,0 +1,57 @@
+package com.DeGuzmanFamilyAPI.DeGuzmanFamilyAPIBackend.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.DeGuzmanFamilyAPI.DeGuzmanFamilyAPIBackend.exception.ResourceNotFoundException;
+import com.DeGuzmanFamilyAPI.DeGuzmanFamilyAPIBackend.models.AutoTransaction;
+import com.DeGuzmanFamilyAPI.DeGuzmanFamilyAPIBackend.service.AutoTransactionService;
+
+@RestController
+@RequestMapping("/app/auto-transactions")
+@CrossOrigin
+public class AutoTransactionController {
+
+	@Autowired
+	private AutoTransactionService autoTransactionService;
+	
+	@GetMapping("/all")
+	public List<AutoTransaction> getAllAutoTransactionInformation() {
+		return autoTransactionService.findAllAutoTransactionInformation();
+	}
+	
+	@GetMapping("/auto-transaction/{autoTransactionId}")
+	public ResponseEntity<AutoTransaction> getAutoTransactionInformationById(@PathVariable Long autoTransactionId) throws ResourceNotFoundException {
+		return autoTransactionService.findAutoTransactionInformationById(autoTransactionId);
+	}
+	
+	@PostMapping("/add-auto-transaction-information")
+	public AutoTransaction addAutoTransactionInfoController(@Valid @RequestBody AutoTransaction autoTransaction) {
+		return autoTransactionService.addAutoTransactionInformation(autoTransaction);
+	}
+	
+	@PutMapping("/auto-transaction/{autoTransactionId}")
+	public ResponseEntity<AutoTransaction> updateAutoTransactionInformationController(@PathVariable Long autoTransactionId,
+			@Valid @RequestBody AutoTransaction autoTransactionDetails) {
+		return autoTransactionService.updateAutoTransactionInformation(autoTransactionId, autoTransactionDetails);
+	}
+	
+	@DeleteMapping("/auto-transaction/{autoTransactionId}")
+	public Map<String,Boolean> deleteAutoTransactionInformationController(@PathVariable Long autoTransactionId) {
+		return autoTransactionService.deleteAutoTransactionInformation(autoTransactionId);
+	}
+}
--- src/main/java/com/DeGuzmanFamilyAPI/DeGuzmanFamilyAPIBackend/controller/TransactionController.java
+++ src/main/java/com/DeGuzmanFamilyAPI/DeGuzmanFamilyAPIBackend/controller/TransactionController.java
@@ -8,8 +8,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,20 +32,23 @@ public class TransactionController {
 		return transactionService.findAllTransactionInformation();
 	}
 	
-	@GetMapping("/transaction")
+	@GetMapping("/transaction/{transactionId}")
 	public ResponseEntity<GeneralTransaction> GetTransactionInformationByID(@PathVariable Long transactionid) throws ResourceNotFoundException {
 		return transactionService.findTransactionInformationByID(transactionid);
 	}
 	
+	@PostMapping("/add-transaction-information")
 	public GeneralTransaction addTransaction(@Valid @RequestBody GeneralTransaction transaction) {
 		return transactionService.addTransactionInformation(transaction);
 	}
 	
+	@PutMapping("/transaction/{transactionId}")
 	public ResponseEntity<GeneralTransaction> updateTransactionInformationController(@PathVariable Long transactionid,
 			@Valid @RequestBody GeneralTransaction transactionDetails) {
 		return transactionService.updateTransactionInformation(transactionid, transactionDetails);
 	}
 	
+	@DeleteMapping("/transaction/{transactionId}")
 	public Map<String,Boolean> deleteTransactionInformation(@PathVariable Long transactionid) {
 		return transactionService.deleteTransactionInformation(transactionid);
 	}
