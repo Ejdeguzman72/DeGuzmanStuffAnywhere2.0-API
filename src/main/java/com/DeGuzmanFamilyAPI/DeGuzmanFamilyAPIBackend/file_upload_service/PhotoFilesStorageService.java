1c23d53a03ad80716c677ce5de36d2c896a1a84e
Ejdeguzman72 <ejdeguzman72@gmail.com>
2020-07-13 00:32:05 -0500

    Photo and video upload backend  functionality

---
+++ src/main/java/com/DeGuzmanFamilyAPI/DeGuzmanFamilyAPIBackend/file_upload_service/PhotoFilesStorageService.java
@@ -0,0 +1,21 @@
+package com.DeGuzmanFamilyAPI.DeGuzmanFamilyAPIBackend.file_upload_service;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import org.springframework.core.io.Resource;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface PhotoFilesStorageService {
+
+	public void init();
+	
+	public void save(MultipartFile file);
+	
+	public org.springframework.core.io.Resource load(String filename);
+	
+	public void deleteAllPhotos();
+	
+	public Stream<Path> loadAllPhotos();
+}
