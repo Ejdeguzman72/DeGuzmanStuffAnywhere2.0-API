2b255253ea05294c1e2ab8585802e07992dc8dca
Ejdeguzman72 <ejdeguzman72@gmail.com>
2020-08-28 01:05:54 -0400

    more changes

--- src/main/java/com/DeGuzmanFamilyAPI/DeGuzmanFamilyAPIBackend/chat_application/WebSocketController.java
+++ src/main/java/com/DeGuzmanFamilyAPI/DeGuzmanFamilyAPIBackend/chat_application/WebSocketController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @CrossOrigin
+//@RequestMapping("/app")
 public class WebSocketController {
 	
 	@MessageMapping("/user-all")
