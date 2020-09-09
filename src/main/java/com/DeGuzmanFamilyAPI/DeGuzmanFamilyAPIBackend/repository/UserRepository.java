bbb24a810b45cff8dced2dec98f19a390d1fc731
Ejdeguzman72 <ejdeguzman72@gmail.com>
2020-09-09 01:36:17 -0400

    find pending users functionality

--- src/main/java/com/DeGuzmanFamilyAPI/DeGuzmanFamilyAPIBackend/repository/UserRepository.java
+++ src/main/java/com/DeGuzmanFamilyAPI/DeGuzmanFamilyAPIBackend/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.DeGuzmanFamilyAPI.DeGuzmanFamilyAPIBackend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.DeGuzmanFamilyAPI.DeGuzmanFamilyAPIBackend.app_models.Users;
@@ -12,4 +13,7 @@ public interface UserRepository extends JpaRepository<Users, Long>{
 
 	Users findUserByUsername(String username);
 	List<Users> findAll();
+	
+	@Query(value = "SELECT * FROM USERS WHERE USER_STATUS = 1", nativeQuery=true)
+	List<Users> findAllPendingUsers();
 }
